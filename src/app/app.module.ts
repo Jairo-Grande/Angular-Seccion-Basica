b822327bf611b71b9fdf0b3f86ee9f1ffbedd9ff
Jairo Grande <jairogrande05@gmail.com>
2021-04-25 09:42:41 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule }         from '@angular/core';
+import { NgModule }          from '@angular/core';
 import { BrowserModule }     from '@angular/platform-browser';
 import { AppComponent }      from './app.component';
-import { ContadorModule } from './contador/contador.module';
-import { HeroesModule }      from './heroes/heroes.module';
+
+import { DbzModule } from './dbz/dbz.module';
+
 
 
 
@@ -12,8 +13,8 @@ import { HeroesModule }      from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule,
-    ContadorModule
+    DbzModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent  {
+
+  nuevo : Personaje={
+  nombre : 'Gohan',
+  poder  : 100
+  } 
+
+  constructor(){
+  }
+
+}
